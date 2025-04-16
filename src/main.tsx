4e541901ef0348bf3ff48e6276c7a9e249a88c87
iahub4metra <buhai.artem7@gmail.com>
2025-04-16 12:53:33 +0100

    react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App/App'
-import { BrowserRouter } from 'react-router'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </StrictMode>,
+);
